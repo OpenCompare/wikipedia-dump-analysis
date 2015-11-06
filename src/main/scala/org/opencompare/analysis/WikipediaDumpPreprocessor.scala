41205d316717a052fb53e65634c9ce3f79f63faa
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-11-06 18:04:21 +0100

    preprocess dump file and read it in spark

---
+++ src/main/scala/org/opencompare/analysis/WikipediaDumpPreprocessor.scala
@@ -0,0 +1,65 @@
+package org.opencompare.analysis
+
+import java.io._
+
+import org.apache.commons.compress.compressors.bzip2.{BZip2CompressorOutputStream, BZip2CompressorInputStream}
+
+import scala.io.Source
+import scala.xml.pull._
+
+/**
+  * Created by gbecan on 06/11/15.
+  */
+class WikipediaDumpPreprocessor {
+
+  def preprocessDump(dumpFile : File, outputFile : File) {
+    // Create compressed input stream
+    val fin = new FileInputStream(dumpFile)
+    val in = new BufferedInputStream(fin)
+    val bzIn = new BZip2CompressorInputStream(in, true)
+
+    // Create XML reader
+    val sourceFile = Source.fromInputStream(bzIn)
+    val xmlReader = new XMLEventReader(sourceFile)
+
+    // Create compressed output stream
+    val fout = new FileOutputStream(outputFile)
+    val out = new BufferedOutputStream(fout)
+    val bzOut = new BZip2CompressorOutputStream(out)
+    val writer = new PrintWriter(bzOut)
+
+
+    val pageBuilder = new StringBuilder
+    var inPage = false
+
+    for (event <- xmlReader) {
+      event match {
+        case EvElemStart(pre, "page", attrs, scope) =>
+          inPage = true
+          pageBuilder ++= "<page>"
+        case EvElemStart(pre, label, attrs, scope) if inPage =>
+          pageBuilder ++= "<" + label + ">"
+        case EvElemStart(pre, label, attrs, scope) =>
+        case EvElemEnd(pre, "page") =>
+          inPage = false
+          pageBuilder ++= "</page>"
+          val revisionText = pageBuilder.toString()
+          writer.print(revisionText.replaceAll("\n","&#10;"))
+          writer.print("\n")
+          pageBuilder.clear()
+        case EvElemEnd(pre, label) if inPage =>
+          pageBuilder ++= "</" + label + ">"
+        case EvElemEnd(pre, label) =>
+        case EvEntityRef(entity) =>
+        case EvProcInstr(target, text) =>
+        case EvText(text) if inPage =>
+          pageBuilder ++= text
+        case EvText(text) =>
+        case EvComment(text) =>
+      }
+    }
+
+    bzIn.close()
+    writer.close()
+  }
+}
--- src/test/scala/org/opencompare/analysis/SparkTest.scala
+++ src/test/scala/org/opencompare/analysis/SparkTest.scala
@@ -3,9 +3,11 @@ package org.opencompare.analysis
 import java.io.File
 
 import akka.actor.ActorSystem
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.scalatest.{FlatSpec, Matchers}
+
+import scala.xml.XML
 
 /**
  * Created by gbecan on 11/5/15.
@@ -13,32 +15,30 @@ import org.scalatest.{FlatSpec, Matchers}
 class SparkTest extends FlatSpec with Matchers {
 
   val articleNamesDumpFile = new File("/home/gbecan/Documents/dev/opencompare/wikipedia-dumps/en/enwiki-20151102-pages-articles-multistream-index.txt.bz2")
-  val dumpFile = new File("/home/gbecan/Documents/dev/opencompare/wikipedia-dumps/en/enwiki-20151102-pages-articles-multistream.xml.bz2")
+  val enDumpFile = new File("/home/gbecan/Documents/dev/opencompare/wikipedia-dumps/en/enwiki-20151102-pages-articles-multistream.xml.bz2")
+  val zuDumpFile = new File("/home/gbecan/Documents/dev/opencompare/wikipedia-dumps/zu/zuwiki-20150806-pages-articles-multistream.xml.bz2")
 
-
-  val actorSystem = ActorSystem("actorSystem")
+  val preprocessedDumpFile = new File("output.xml.bz2")
 
   val sparkConf = new SparkConf()
     .setAppName("wikipedia-analysis")
     .setMaster("local[4]")
-//  val sparkContext = new SparkContext(sparkConf)
+
+    val sparkContext = new SparkContext(sparkConf)
 //  val sparkStreamingContext = new StreamingContext(sparkConf, Seconds(1))
 //  val sqlContext = new SQLContext(sparkContext)
 
   it should "do stuff" in {
 
-    val searchedString = "Comparison of "
+    val dumpPreprocessor = new WikipediaDumpPreprocessor
+    dumpPreprocessor.preprocessDump(zuDumpFile, preprocessedDumpFile)
 
-    val dumpReaderActor = actorSystem.actorOf(WikipediaDumpReader.props(dumpFile), "DumpReader")
-    println(dumpReaderActor.path.toSerializationFormat)
-//    val nbOfTables = sparkStreamingContext.actorStream[Int](DumpReceiver.props(dumpReaderActor.path), "DumpReceiver")
-//
-//    dumpReaderActor ! NextPage()
-//
-//    nbOfTables.foreachRDD(rdd => rdd.collect().foreach(println))
-//
-//    sparkStreamingContext.start()
-//    sparkStreamingContext.awaitTermination()
+    val xmlDocs = sparkContext
+      .textFile(preprocessedDumpFile.getAbsolutePath)
+      .map(doc => XML.loadString(doc))
+      .first()
+
+    println(xmlDocs)
 
   }
 
